8e0aa131c7dbf1cb6da95339687444ed946299f8
AnkirPandey <developerankitpandey@gmail.com>
2024-04-16 11:29:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Socialmedia from './Social-media/Socialmedia';
+
+function App() {
+  return (
+    <>
+    <Socialmedia/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Social-media/Socialmedia.jsx
@@ -0,0 +1,232 @@
+import React from 'react'
+import './Socialmedia.css'
+
+const Socialmedia = () => {
+    return (
+        <>
+            {/* <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet"> */}
+
+            <div class="device">
+                <div class="instagram">
+
+                    <header class="top">
+                        <div class="head-btn">
+                            <span class="photo">&nbsp;</span>
+                        </div>
+                        <div class="head-logo">
+                            <span class="logo">&nbsp;</span>
+                        </div>
+                        <div class="head-btn">
+                            <span class="people">&nbsp;</span>
+                        </div>
+                    </header>
+
+                    <div class="scroll">
+
+                        <section class="stories">
+
+                            <div class="scroll-stories">
+
+                                <div class="storie">
+                                    <span class="photo">
+                                        <img src="https://storage.googleapis.com/mkts/walter.jpg" alt="Fulano" />
+                                    </span>
+                                    <span class="name">Walter</span>
+                                </div>
+                                <div class="storie">
+                                    <span class="photo">
+                                        <img src="https://storage.googleapis.com/mkts/walter.jpg" alt="Fulano" />
+                                    </span>
+                                    <span class="name">Walter</span>
+                                </div>
+                                <div class="storie">
+                                    <span class="photo">
+                                        <img src="https://storage.googleapis.com/mkts/walter.jpg" alt="Fulano" />
+                                    </span>
+                                    <span class="name">Walter</span>
+                                </div>
+                                <div class="storie">
+                                    <span class="photo">
+                                        <img src="https://storage.googleapis.com/mkts/walter.jpg" alt="Fulano" />
+                                    </span>
+                                    <span class="name">Walter</span>
+                                </div>
+                                <div class="storie">
+                                    <span class="photo">
+                                        <img src="https://storage.googleapis.com/mkts/walter.jpg" alt="Fulano" />
+                                    </span>
+                                    <span class="name">Walter</span>
+                                </div>
+                                <div class="storie">
+                                    <span class="photo">
+                                        <img src="https://storage.googleapis.com/mkts/walter.jpg" alt="Fulano" />
+                                    </span>
+                                    <span class="name">Walter</span>
+                                </div>
+                                <div class="storie">
+                                    <span class="photo">
+                                        <img src="https://storage.googleapis.com/mkts/walter.jpg" alt="Fulano" />
+                                    </span>
+                                    <span class="name">Walter</span>
+                                </div>
+                                <div class="storie">
+                                    <span class="photo">
+                                        <img src="https://storage.googleapis.com/mkts/walter.jpg" alt="Fulano" />
+                                    </span>
+                                    <span class="name">Walter</span>
+                                </div>
+
+                            </div>
+
+                        </section>
+
+                        <main class="content">
+
+                            <div class="post">
+                                <div class="post-header">
+                                    <img class="user-thumb" src="https://storage.googleapis.com/mkts/walter.jpg" alt="Walter" />
+                                    <div class="user-details">
+                                        <strong class="name">Walter</strong>
+                                        <span class="location">Busca Vida, BA</span>
+                                    </div>
+                                    <div class="post-menu">
+                                        <span class="menu">&nbsp;</span>
+                                    </div>
+                                </div>
+                                <img src="https://image.freepik.com/psd-gratuitas/instagram-post-mockup_15879-4.jpg" alt="post" class="post-photo" />
+                                <div class="post-footer">
+
+                                    <div class="buttons">
+                                        <div class="post-btn"><span class="like">&nbsp;</span></div>
+                                        <div class="post-btn"><span class="comn">&nbsp;</span></div>
+                                        <div class="spacer">&nbsp;</div>
+                                        <div class="post-btn"><span class="save">&nbsp;</span></div>
+                                    </div>
+
+                                    <div class="likes">
+                                        <img src="https://storage.googleapis.com/mkts/walter.jpg" alt="user" class="user-like" />
+                                        Curtido por <strong>Fulano</strong> e <strong>outras 8 pessoas</strong>
+                                    </div>
+
+                                    <div class="comments">
+                                        <p>
+                                            <strong>walter</strong> Uma tela projetada para o instagram
+                                        </p>
+                                        <p>
+                                            <strong>beltrano</strong> Ficou show de bola man!
+                                        </p>
+                                    </div>
+
+                                    <span class="time">há 20 horas</span>
+
+                                </div>
+                            </div>
+
+                            <div class="post">
+                                <div class="post-header">
+                                    <img class="user-thumb" src="https://storage.googleapis.com/mkts/walter.jpg" alt="Walter" />
+                                    <div class="user-details">
+                                        <strong class="name">Walter</strong>
+                                        <span class="location">Busca Vida, BA</span>
+                                    </div>
+                                    <div class="post-menu">
+                                        <span class="menu">&nbsp;</span>
+                                    </div>
+                                </div>
+                                <img src="https://image.freepik.com/psd-gratuitas/instagram-post-mockup_15879-4.jpg" alt="post" class="post-photo" />
+                                <div class="post-footer">
+
+                                    <div class="buttons">
+                                        <div class="post-btn"><span class="like">&nbsp;</span></div>
+                                        <div class="post-btn"><span class="comn">&nbsp;</span></div>
+                                        <div class="spacer">&nbsp;</div>
+                                        <div class="post-btn"><span class="save">&nbsp;</span></div>
+                                    </div>
+
+                                    <div class="likes">
+                                        <img src="https://storage.googleapis.com/mkts/walter.jpg" alt="user" class="user-like" />
+                                        Curtido por <strong>Fulano</strong> e <strong>outras 8 pessoas</strong>
+                                    </div>
+
+                                    <div class="comments">
+                                        <p>
+                                            <strong>walter</strong> Uma tela projetada para o instagram
+                                        </p>
+                                        <p>
+                                            <strong>beltrano</strong> Ficou show de bola man!
+                                        </p>
+                                    </div>
+
+                                    <span class="time">há 20 horas</span>
+
+                                </div>
+                            </div>
+                            <div class="post">
+                                <div class="post-header">
+                                    <img class="user-thumb" src="https://storage.googleapis.com/mkts/walter.jpg" alt="Walter" />
+                                    <div class="user-details">
+                                        <strong class="name">Walter</strong>
+                                        <span class="location">Busca Vida, BA</span>
+                                    </div>
+                                    <div class="post-menu">
+                                        <span class="menu">&nbsp;</span>
+                                    </div>
+                                </div>
+                                <img src="https://image.freepik.com/psd-gratuitas/instagram-post-mockup_15879-4.jpg" alt="post" class="post-photo" />
+                                <div class="post-footer">
+
+                                    <div class="buttons">
+                                        <div class="post-btn"><span class="like">&nbsp;</span></div>
+                                        <div class="post-btn"><span class="comn">&nbsp;</span></div>
+                                        <div class="spacer">&nbsp;</div>
+                                        <div class="post-btn"><span class="save">&nbsp;</span></div>
+                                    </div>
+
+                                    <div class="likes">
+                                        <img src="https://storage.googleapis.com/mkts/walter.jpg" alt="user" class="user-like" />
+                                        Curtido por <strong>Fulano</strong> e <strong>outras 8 pessoas</strong>
+                                    </div>
+
+                                    <div class="comments">
+                                        <p>
+                                            <strong>walter</strong> Uma tela projetada para o instagram
+                                        </p>
+                                        <p>
+                                            <strong>beltrano</strong> Ficou show de bola man!
+                                        </p>
+                                    </div>
+
+                                    <span class="time">há 20 horas</span>
+
+                                </div>
+                            </div>
+
+                        </main>
+
+                    </div>
+
+                    <footer class="bottom">
+                        <a class="footer-btn">
+                            <span class="home">&nbsp;</span>
+                        </a>
+                        <a class="footer-btn">
+                            <span class="search">&nbsp;</span>
+                        </a>
+                        <a class="footer-btn">
+                            <span class="add">&nbsp;</span>
+                        </a>
+                        <a class="footer-btn">
+                            <span class="like">&nbsp;</span>
+                        </a>
+                        <a class="footer-btn">
+                            <span class="profile">&nbsp;</span>
+                        </a>
+                    </footer>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Socialmedia;
